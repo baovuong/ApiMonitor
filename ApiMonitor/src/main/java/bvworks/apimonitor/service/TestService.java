830806d967d03da36126fe12d5c8251182bbd436
Bao Vuong <vqbao@lv5.org>
2015-02-05 04:36:40 -0500

    updated java version

--- ApiMonitor/src/main/java/bvworks/apimonitor/service/TestService.java
+++ ApiMonitor/src/main/java/bvworks/apimonitor/service/TestService.java
@@ -36,8 +36,9 @@ public class TestService {
 		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
 
 		try (OutputStream output = connection.getOutputStream()) {
-		    output.write(query.getBytes(charset));
+			output.write(query.getBytes(charset));			
 		}
+
 		
 		InputStream response = connection.getInputStream();
 		
