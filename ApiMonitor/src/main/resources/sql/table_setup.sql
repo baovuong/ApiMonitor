42ff829231ee09585c7fef92c72146b6c363eebc
Bao Vuong <vqbao@lv5.org>
2015-02-12 05:07:07 -0500

    added id in locations table

--- ApiMonitor/src/main/resources/sql/table_setup.sql
+++ ApiMonitor/src/main/resources/sql/table_setup.sql
@@ -9,9 +9,10 @@ PRIMARY KEY (id)
 
 CREATE TABLE IF NOT EXISTS locations
 (
+id INT NOT NULL AUTO_INCREMENT,
 ip_address CHAR(45) NOT NULL,
 city VARCHAR(30),
 region VARCHAR(30),
 country VARCHAR(30),
-PRIMARY KEY(ip_address)
+PRIMARY KEY(id)
 );
