492cad3f42c449e7850b05d68e121e534b4a067e
Bao Vuong <vqbao@lv5.org>
2015-02-12 04:35:53 -0500

    new table for storing locations

--- ApiMonitor/src/main/resources/sql/table_setup.sql
+++ ApiMonitor/src/main/resources/sql/table_setup.sql
@@ -6,3 +6,12 @@ name CHAR(30),
 ip_address CHAR(45),
 PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS locations
+(
+ip_address CHAR(45) NOT NULL,
+city VARCHAR(30),
+region VARCHAR(30),
+country VARCHAR(30),
+PRIMARY KEY(ip_address)
+);
